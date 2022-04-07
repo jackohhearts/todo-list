3d679574840481d9df17ee1eab4de19bafa804c8
Joseph Campbell <thecanadianjoe@gmail.com>
2022-04-07 12:12:09 -0400

    quit feature established

---
+++ app.js
@@ -0,0 +1,7 @@
+let input = prompt("What would you like to do?");
+const todos = ["Test", "Test 2"];
+while (input !== "quit" && input !== "q") {
+    input = prompt("What would you like to do?")
+}
+
+console.log("Okay, quitting the app!")
